67c8f11717d49194684cc2990773e8219726f1cd
Ananya <ananya19102000@gmail.com>
2024-02-01 13:39:52 +0530

    created pages and routes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,8 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/App.jsx",
+    "./src/components/Header.jsx",
   ],
   theme: {
     extend: {},
